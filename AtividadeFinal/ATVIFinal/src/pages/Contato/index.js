797a9243e4eca3e43e1889e97d0ff3084e248aa0
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2024-12-05 08:39:30 -0300

    atvFinal

---
+++ AtividadeFinal/ATVIFinal/src/pages/Contato/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Contato() {
+  return (
+    <View>
+      <Text>Contato</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   text: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   },
+// });
---
+++ AtividadeFinal/ATVIFinal/src/pages/Detalhes/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Detalhes() {
+  return (
+    <View>
+      <Text>Página detalhes do usuario</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   text: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   },
+// });
